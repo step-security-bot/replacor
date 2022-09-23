08c7b1ec5574f02c7e51d1521e71778ebea94d9e
SKi <megamanics@users.noreply.github.com>
2022-09-23 15:00:11 -0700

    fix eslint

--- replace.js
+++ replace.js
@@ -1,132 +1,188 @@
 #!/usr/bin/env node
 
-import fetch from "node-fetch";
+import fetch from 'node-fetch'
 import dotenv from 'dotenv'
-import {Buffer} from 'node:buffer';
-import {Command, Option} from 'commander';
-import log4js from 'log4js';
-import {diffWordsWithSpace} from 'diff';
-import colors from 'colors';
+import { Buffer } from 'node:buffer'
+import { Command, Option } from 'commander'
+import log4js from 'log4js'
+import { diffWordsWithSpace } from 'diff'
+import colors from 'colors'
 
-let logger = log4js.getLogger();
-dotenv.config();
+const logger = log4js.getLogger()
+dotenv.config()
 
 const options = new Command()
-    .name('replace.js')
-    .description('CLI to replace strings in Confluence Pages')
-    .version('0.0.1')
-    .requiredOption('-q, --query <query>', 'CQL query used to search pages, eg: text~gitlab')
-    .requiredOption('-s, --search  <string>', 'string to replace eg: gitlab')
-    .requiredOption('-r, --replace  <string>', 'replacement string eg: gitlab -> github')
-    .addOption(new Option('-u, --user  <user>', 'user eg: your_email@domain.com').env('CONFLUENCE_USER'))
-    .addOption(new Option('-t, --token <token>', 'your_user_api_token with scope read:content-details:confluence,write:content:confluence').env('CONFLUENCE_TOKEN'))
-    .addOption(new Option('-d, --domain <domainurl>', 'eg: https://<domain_name>.atlassian.net').env('CONFLUENCE_DOMAIN'))
-    .addOption(new Option('-l, --loglevel <loglevel>', 'loglevel, eg: debug, info, warn, error, fatal').env('LOGLEVEL'))
-    .option('-c, --convertbburl', 'convert bitbucket url format to github url format')
-    .option('--dryrun', 'dry run only')
-    .parse()
-    .opts();
+  .name('replace.js')
+  .description('CLI to replace strings in Confluence Pages')
+  .version('0.0.1')
+  .requiredOption(
+    '-q, --query <query>',
+    'CQL query used to search pages, eg: text~gitlab'
+  )
+  .requiredOption('-s, --search  <string>', 'string to replace eg: gitlab')
+  .requiredOption(
+    '-r, --replace  <string>',
+    'replacement string eg: gitlab -> github'
+  )
+  .addOption(
+    new Option('-u, --user  <user>', 'user eg: your_email@domain.com').env(
+      'CONFLUENCE_USER'
+    )
+  )
+  .addOption(
+    new Option(
+      '-t, --token <token>',
+      'your_user_api_token with scope read:content-details:confluence,write:content:confluence'
+    ).env('CONFLUENCE_TOKEN')
+  )
+  .addOption(
+    new Option(
+      '-d, --domain <domainurl>',
+      'eg: https://<domain_name>.atlassian.net'
+    ).env('CONFLUENCE_DOMAIN')
+  )
+  .addOption(
+    new Option(
+      '-l, --loglevel <loglevel>',
+      'loglevel, eg: debug, info, warn, error, fatal'
+    ).env('LOGLEVEL')
+  )
+  .option(
+    '-c, --convertbburl',
+    'convert bitbucket url format to github url format'
+  )
+  .option('--dryrun', 'dry run only')
+  .parse()
+  .opts()
 
-let user, token, query, domain, search, replacestr = "";
-replacestr = options.replace;
-query = options.query;
-search = options.search;
-user = options.user;
-token = options.token;
-domain = options.domain;
+let user
+let token
+let query
+let domain
+let search
+let replacestr = ''
+replacestr = options.replace
+query = options.query
+search = options.search
+user = options.user
+token = options.token
+domain = options.domain
 
-logger.level = options.loglevel ?? process.env.LOGLEVEL ?? 'info';
-logger.debug(options);
+logger.level = options.loglevel ?? process.env.LOGLEVEL ?? 'info'
+logger.debug(options)
 
-const searchQuery = domain + "/wiki/rest/api/content/search?cql=" + query;
+const searchQuery = domain + '/wiki/rest/api/content/search?cql=' + query
 
 const header = {
-    'Content-Type': 'application/json',
-    'Authorization': `Basic ` + Buffer.from(user + `:` + token).toString('base64')
-};
-logger.debug({"searchQuery: ": searchQuery});
-logger.debug(header);
+  'Content-Type': 'application/json',
+  Authorization: 'Basic ' + Buffer.from(user + ':' + token).toString('base64')
+}
+logger.debug({ 'searchQuery: ': searchQuery })
+logger.debug(header)
 
 fetch(searchQuery, {
+  method: 'GET',
+  headers: header
+})
+  .then((res) => res.json())
+  .then((json) => {
+    logger.debug({ 'Content IDs: ': json.results.map((result) => result.id) })
+    json.size
+      ? console.table({ 'Total Pages: ': json.size })
+      : console.table({ 'No results matching query': query })
+    json.results.forEach((result) => {
+      getContent(result.id)
+    })
+  })
+  .catch((err) => {
+    console.error(err)
+  })
+
+function getPageQuery (pageId) {
+  return (
+    domain + '/wiki/rest/api/content/' + pageId + '?expand=body.storage,version'
+  )
+}
+
+function getPageUpdateQuery (pageId) {
+  return domain + '/wiki/rest/api/content/' + pageId
+}
+
+function convertBBurl2GH (content) {
+  const re = /projects\/REP\/repos\/([^\/]*)\/browse/gim
+  const subst = '$1'
+  return content.replace(re, subst)
+}
+
+function replaceStr (content) {
+  const replacedContent = content.replace(new RegExp(search, 'ig'), replacestr)
+  const replacedContent2 = options.convertbburl
+    ? convertBBurl2GH(replacedContent)
+    : replacedContent
+  return replacedContent2
+}
+
+function showdiff (content, replacedContent) {
+  if (content != replacedContent) {
+    const dff = diffWordsWithSpace(content, replacedContent, {
+      ignoreCase: true,
+      ignoreWhitespace: true
+    })
+    let diffstr = ''
+    dff.forEach((part) => {
+      const color = part.added ? 'green' : part.removed ? 'red' : 'grey'
+      // console.log(colors[color](part?.value));
+      diffstr += colors[color](part?.value)
+      // process.stdout.write(colors[color](part?.value ?? ""));
+    })
+    console.log('')
+    return diffstr
+  } else {
+    return 'no changes'
+  }
+}
+
+function getContent (id) {
+  fetch(getPageQuery(id), {
     method: 'GET',
     headers: header
-}).then(res => res.json()).then(json => {
-    logger.debug({"Content IDs: ": json.results.map(result => result.id)});
-    json.size ? console.table({"Total Pages: ": json.size}) : console.table({"No results matching query": query});
-    json.results.forEach(result => {
-        getContent(result.id);
-    });
-}).catch(err => {
-    console.error(err);
-})
-
-function getPageQuery(pageId) {
-    return domain + "/wiki/rest/api/content/" + pageId + "?expand=body.storage,version";
+  })
+    .then((res) => res.json())
+    .then((json) => {
+      const content = json.body.storage.value
+      const title = json.title
+      const type = json.type
+      const replacedContent = replaceStr(content)
+      const titleReplaced = replaceStr(title)
+      logger.info({ 'Page ID: ': id, 'Title: ': title, 'Type: ': type })
+      const difftitle = showdiff(title, titleReplaced)
+      const diffstr = showdiff(content, replacedContent)
+      process.stdout.write(diffstr) && console.log('')
+      options.dryrun
+        ? logger.debug({
+          title,
+          ...(title == titleReplaced && { 'No changes to title': '' }),
+          ...(content != replacedContent && {
+            content,
+            replacedContent
+          }),
+          ...(content == replacedContent && { 'No changes to content': '' })
+        })
+        : updateContent(
+          id,
+          json.version.number + 1,
+          type,
+          titleReplaced,
+          JSON.stringify(replacedContent)
+        )
+    })
+    .catch((err) => {
+      console.error(err)
+    })
 }
 
-function getPageUpdateQuery(pageId) {
-    return domain + "/wiki/rest/api/content/" + pageId;
-}
-
-function convertBBurl2GH(content) {
-    var re = /projects\/REP\/repos\/([^\/]*)\/browse/gmi;
-    var subst = '$1';
-    return content.replace(re, subst);
-}
-
-function replaceStr(content) {
-    let replacedContent = content.replace(new RegExp(search, 'ig'), replacestr);
-    let replacedContent2 = options.convertbburl ? convertBBurl2GH(replacedContent) : replacedContent;
-    return replacedContent2;
-}
-
-function showdiff(content, replacedContent) {
-    if (content != replacedContent) {
-        const dff = diffWordsWithSpace(content, replacedContent, {ignoreCase: true, ignoreWhitespace: true});
-        var diffstr = "";
-        dff.forEach((part) => {
-            const color = part.added ? 'green' : part.removed ? 'red' : 'grey';
-            //console.log(colors[color](part?.value));
-            diffstr += colors[color](part?.value);
-            //process.stdout.write(colors[color](part?.value ?? ""));
-        });
-        console.log("");
-        return diffstr;
-    } else {
-        return "no changes";
-    }
-}
-
-function getContent(id) {
-    fetch(getPageQuery(id), {
-        method: 'GET',
-        headers: header
-    }).then(res => res.json())
-        .then(json => {
-            let content = json.body.storage.value;
-            let title = json.title;
-            let type = json.type;
-            let replacedContent = replaceStr(content);
-            let titleReplaced = replaceStr(title);
-            logger.info({"Page ID: ": id, "Title: ": title, "Type: ": type});
-            let difftitle = showdiff(title, titleReplaced);
-            let diffstr = showdiff(content, replacedContent);
-            process.stdout.write(diffstr) && console.log("");
-            options.dryrun ? logger.debug({
-                title, ...(title == titleReplaced && {"No changes to title": ""}), ...(content != replacedContent && {
-                        content,
-                        replacedContent
-                    }
-                ),
-                ...(content == replacedContent && {"No changes to content": ""})
-            }) : updateContent(id, json.version.number + 1, type, titleReplaced, JSON.stringify(replacedContent));
-        }).catch(err => {
-        console.error(err);
-    });
-};
-
-function updateContent(id, version, type, title, content) {
-    let bodyData = `{
+function updateContent (id, version, type, title, content) {
+  const bodyData = `{
         "version": {
             "number": ${version}
         },
@@ -138,22 +194,25 @@ function updateContent(id, version, type, title, content) {
                 "representation": "storage"
             }
         }
-    }`;
-    logger.debug(bodyData.toString());
-    fetch(getPageUpdateQuery(id), {
-        method: 'PUT',
-        headers: header,
-        body: bodyData
-    }).then(res => res.json())
-        .then(json => {
-            json.data ? console.log(json.data.errors) : console.table({
-                "id": json.id,
-                "type": json.type,
-                "version": json.version.number,
-                "title": json.title
-            })
-        }).catch(err => {
-        console.error(err);
-    });
-};
-
+    }`
+  logger.debug(bodyData.toString())
+  fetch(getPageUpdateQuery(id), {
+    method: 'PUT',
+    headers: header,
+    body: bodyData
+  })
+    .then((res) => res.json())
+    .then((json) => {
+      json.data
+        ? console.log(json.data.errors)
+        : console.table({
+          id: json.id,
+          type: json.type,
+          version: json.version.number,
+          title: json.title
+        })
+    })
+    .catch((err) => {
+      console.error(err)
+    })
+}
--- search.js
+++ search.js
@@ -1,62 +1,64 @@
 #!/usr/bin/env node
 
-import fetch from "node-fetch";
+import fetch from 'node-fetch'
 import dotenv from 'dotenv'
-import {Buffer} from 'node:buffer';
-import {Command} from 'commander';
+import { Buffer } from 'node:buffer'
+import { Command } from 'commander'
 
-dotenv.config();
+dotenv.config()
 
-console.debug = process.env.DEBUG ? console.table : () => {
-};
+console.debug = process.env.DEBUG
+  ? console.table
+  : () => {
+    }
 
 const options = new Command()
-    .name('search.js')
-    .description('CLI to search Confluence Pages')
-    .version('0.0.1')
-    .requiredOption('-q, --query <query>', 'CQL query to search for, eg: text~gitlab')
-    .requiredOption('-u, --user  <user>', 'user eg: your_email@domain.com')
-    .requiredOption('-t, --token <token>', 'your_user_api_token with scope read:content-details:confluence,write:content:confluence')
-    .requiredOption('-d, --domain <domainurl>', 'eg: https://<domain_name>.atlassian.net')
-    .parse()
-    .opts();
+  .name('search.js')
+  .description('CLI to search Confluence Pages')
+  .version('0.0.1')
+  .requiredOption('-q, --query <query>', 'CQL query to search for, eg: text~gitlab')
+  .requiredOption('-u, --user  <user>', 'user eg: your_email@domain.com')
+  .requiredOption('-t, --token <token>', 'your_user_api_token with scope read:content-details:confluence,write:content:confluence')
+  .requiredOption('-d, --domain <domainurl>', 'eg: https://<domain_name>.atlassian.net')
+  .parse()
+  .opts()
 
-console.debug(options);
+console.debug(options)
 
-let user, token, query, domain = "";
-query = options.query;
-user = options.user ? user = options.user : user = process.env.CONFLUENCE_USER;
-token = options.token ? token = options.token : token = process.env.CONFLUENCE_TOKEN;
-domain = options.domain ? domain = options.domain : domain = process.env.CONFLUENCE_DOMAIN;
+let user; let token; let query; let domain = ''
+query = options.query
+user = options.user ? user = options.user : user = process.env.CONFLUENCE_USER
+token = options.token ? token = options.token : token = process.env.CONFLUENCE_TOKEN
+domain = options.domain ? domain = options.domain : domain = process.env.CONFLUENCE_DOMAIN
 
-const searchQuery = domain + "/wiki/rest/api/content/search?cql=" + query;
+const searchQuery = domain + '/wiki/rest/api/content/search?cql=' + query
 const header = {
-    'Content-Type': 'application/json',
-    'Authorization': `Basic ` + Buffer.from(user + `:` + token).toString('base64')
-};
+  'Content-Type': 'application/json',
+  Authorization: 'Basic ' + Buffer.from(user + ':' + token).toString('base64')
+}
 
-console.table({searchQuery});
-console.debug(header);
+console.table({ searchQuery })
+console.debug(header)
 
 fetch(searchQuery, {
-    method: 'GET',
-    headers: header
+  method: 'GET',
+  headers: header
 }).then(res => res.json())
-    .then(json => {
-        if (json.results != null) {
-            let hm = []
-            json.results.forEach(result => {
-                hm.push({
-                    "id": result.id,
-                    "type": result.type,
-                    "url": domain + "/wiki" + result._links.tinyui,
-                    "title": result.title
-                })
-            });
-            json.size ? console.table(hm) : console.table({"No results matching query": query});
-        } else {
-            console.error(json.message);
-        }
-    }).catch(err => {
-    console.error(err);
-})
+  .then(json => {
+    if (json.results != null) {
+      const hm = []
+      json.results.forEach(result => {
+        hm.push({
+          id: result.id,
+          type: result.type,
+          url: domain + '/wiki' + result._links.tinyui,
+          title: result.title
+        })
+      })
+      json.size ? console.table(hm) : console.table({ 'No results matching query': query })
+    } else {
+      console.error(json.message)
+    }
+  }).catch(err => {
+    console.error(err)
+  })
